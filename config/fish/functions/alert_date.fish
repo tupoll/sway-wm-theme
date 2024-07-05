68970e3076e47d3bfdb8549304e6cf0ade2a5b66
tupoll <tupollgbt@gmail.com>
2024-07-05 23:47:59 +1000

    Alias fish

---
+++ config/fish/functions/alert_date.fish
@@ -0,0 +1,4 @@
+function alert_date --wraps='lua $HOME/.config/sway/modules/alert/alert_date.lua' --description 'alias alert_date lua HOME/.config/sway/modules/alert/alert_date.lua'
+  lua $HOME/.config/sway/modules/alert/alert_date.lua $argv
+        
+end
