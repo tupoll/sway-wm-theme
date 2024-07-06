142a906ec91d26ca760276ac588c767b33e7d5a2
tupoll <tupollgbt@gmail.com>
2024-07-06 15:00:26 +1000

    Script lua cmd.

---
+++ config/sway/modules/alert/cmd/alert_date.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/lua
+os.execute('pkill -f $HOME/.config/sway/modules/alert/alert_time.lua')
+io.popen("lua $HOME/.config/sway/modules/alert/alert_date.lua")
+local a = ("#!/usr/bin/lua\n")
+local b = ('io.popen("lua $HOME/.config/sway/modules/alert/cmd/alert_date_kil.lua ")')
+local c = ('io.popen("pkill -f mako && pkill -f alert_date")')
+vol = io.open(".config/sway/modules/alert/alert_date_ch.lua", "w")
+vol:write(a, c, b)
+vol:close()
