ad58d8801548f569cff209c4fb3c5ada2a3d6b98
tupoll <tupollgbt@gmail.com>
2024-07-07 19:01:00 +1000

    Workspace iptv

--- config/sway/modules/tags/mpv_t9.lua
+++ config/sway/modules/tags/mpv_t9.lua
@@ -2,7 +2,7 @@
 
 local function tag(sway) os.execute("fish -c tag9")end 
 local function layout() os.execute("sway layout splith")end
-local function mpv() os.execute("mpv http://ec106815c7b8.zatikov.net/playlists/uplist/fc78243accca2f9c39fe17efcb486b25/playlist.m3u8 &")end
+local function mpv() os.execute("mpv ")end
 tag()
 mpv()
 layout()
