a614a1b53eb8cf777321e8f3418596172b773c20
tupoll <tupollgbt@gmail.com>
2024-07-09 19:26:05 +1000

    tag 8

--- config/sway/modules/tags/tag8.lua
+++ config/sway/modules/tags/tag8.lua
@@ -5,7 +5,7 @@ local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
 
 local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
 
-local function tag_1(sway) os.execute("sway  workspace ️8️⃣ media && setwallpaper $HOME/.config/sway/pictures/tag8.jpg &")end 
+local function tag_1(sway) os.execute("sway  workspace ️8️⃣ media && setwallpaper /var/tmp/sway/pictures/tag8.jpg &")end 
 local function layout1() os.execute("fish -c layout_stacking") end
                                                                                     
 tag_1()       
