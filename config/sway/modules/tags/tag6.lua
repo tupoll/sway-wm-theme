839b9039b72be90a9719c4c78658656d1c4680ed
tupoll <tupollgbt@gmail.com>
2024-07-09 19:22:41 +1000

    tag 6

--- config/sway/modules/tags/tag6.lua
+++ config/sway/modules/tags/tag6.lua
@@ -5,7 +5,7 @@ local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
 
 local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
 
-local function tag_1(sway) os.execute("sway  workspace ️6️⃣ office && setwallpaper $HOME/.config/sway/pictures/tag6.jpg &")end 
+local function tag_1(sway) os.execute("sway  workspace ️6️⃣ office && setwallpaper /var/tmp/sway/pictures/tag6.jpg &")end 
 local function layout1() os.execute("fish -c layout_tabbed") end
                                                                                     
 tag_1()       
