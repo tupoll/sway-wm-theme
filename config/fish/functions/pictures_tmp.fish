d68ba3852adc531b4d093d74d6bedea9ffeac993
tupoll <tupollgbt@gmail.com>
2024-07-09 19:08:23 +1000

    Temp copy rule

---
+++ config/fish/functions/pictures_tmp.fish
@@ -0,0 +1,4 @@
+function pictures_tmp --wraps='cp -R .config/sway/pictures /var/tmp/sway/pictures' --description 'alias pictures_tmp cp -R .config/sway/pictures /var/tmp/sway/pictures'
+  cp -R .config/sway/pictures /var/tmp/sway/pictures $argv
+        
+end
