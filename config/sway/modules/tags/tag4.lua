5cc0981239ce2532fc69b677f169a39aaae4f694
tupoll <tupollgbt@gmail.com>
2024-07-09 19:19:38 +1000

    tag 4

--- config/sway/modules/tags/tag4.lua
+++ config/sway/modules/tags/tag4.lua
@@ -5,7 +5,7 @@ local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
 
 local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
 
-local function tag_1(sway) os.execute("sway  workspace 4️⃣ editor && setwallpaper $HOME/.config/sway/pictures/tag4.jpg &")end 
+local function tag_1(sway) os.execute("sway  workspace 4️⃣ editor && setwallpaper /var/tmp/sway/pictures/tag4.jpg &")end 
 local function layout1() os.execute("fish -c layout_splitv") end
                                                                                     
 tag_1()       
