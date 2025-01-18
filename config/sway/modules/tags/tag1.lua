b1206574e1dabce3ddbeb47c409f03a32ae7ff92
tupoll <tupollgbt@gmail.com>
2025-01-18 19:28:53 +1000

    tag1.lua

--- config/sway/modules/tags/tag1.lua
+++ config/sway/modules/tags/tag1.lua
@@ -1,11 +1,6 @@
 #!/usr/bin/lua
-local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
-
-
-local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
-
-local function tag_1(sway) os.execute("sway workspace 1️⃣ work && setwallpaper /var/tmp/sway/pictures/tag1.jpg &")end 
-local function layout1() os.execute("fish -c layout_splith") end
+local function tag_1(sway) os.execute("sway workspace 1️⃣ fm && setwallpaper /var/tmp/sway/pictures/tag1.jpg &")end 
+local function layout1() os.execute("fish -c layout_tabbed") end
                                                                                     
 tag_1()       
 layout1()
