b8a1924973d3f347d3126eb2c7d9e337664768e2
tupoll <tupollgbt@gmail.com>
2025-01-18 21:00:47 +1000

    Delete config/fish/functions/thunar_t3.fish

--- config/fish/functions/thunar_t3.fish
+++
@@ -1,4 +0,0 @@
-function thunar_t3 --wraps=$HOME/.config/sway/modules/tags/thunar_t3.lua --description 'alias thunar_t3 $HOME/.config/sway/modules/tags/thunar_t3.lua'
-  $HOME/.config/sway/modules/tags/thunar_t3.lua $argv
-        
-end
