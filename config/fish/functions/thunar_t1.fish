7b51586a68f8b0ac063297d16721d02cb7380fae
tupoll <tupollgbt@gmail.com>
2025-01-18 21:03:02 +1000

    Alias fish.

---
+++ config/fish/functions/thunar_t1.fish
@@ -0,0 +1,4 @@
+function thunar_t1 --wraps=$HOME/.config/sway/modules/tags/thunar_t1.lua --description 'alias thunar_t1 $HOME/.config/sway/modules/tags/thunar_t1.lua'
+  $HOME/.config/sway/modules/tags/thunar_t1.lua $argv
+        
+end
