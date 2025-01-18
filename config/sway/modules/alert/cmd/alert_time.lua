1cc9181b3006f178fff25b696d3cdbcabb19de3d
tupoll <tupollgbt@gmail.com>
2025-01-18 18:49:52 +1000

    Update alert_time.lua

--- config/sway/modules/alert/cmd/alert_time.lua
+++ config/sway/modules/alert/cmd/alert_time.lua
@@ -2,7 +2,7 @@
 io.popen("lua $HOME/.config/sway/modules/alert/alert_time.lua")
 local a = ("#!/usr/bin/lua\n")
 local b = ('io.popen("lua $HOME/.config/sway/modules/alert/cmd/alert_date.lua")\n')
-local c = ('io.popen("pkill -f mako && pkill -f alert_time ")')
+local c = ('io.popen("pkill -f nwg-wrapper && pkill -f alert_time ")')
 vol = io.open(".config/sway/modules/alert/alert_date_ch.lua", "w")
 vol:write(a, c, b)
 vol:close()
