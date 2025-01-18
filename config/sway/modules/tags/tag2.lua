40de87941106d37850065269da88205c1a159c40
tupoll <tupollgbt@gmail.com>
2025-01-18 19:30:46 +1000

    tag2.lua

--- config/sway/modules/tags/tag2.lua
+++ config/sway/modules/tags/tag2.lua
@@ -1,8 +1,4 @@
-local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
-
-
-local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
-
+#!/usr/bin/lua
 local function tag_2(sway) os.execute("sway  workspace 2️⃣ browser && setwallpaper /var/tmp/sway/pictures/tag2.jpg &")end 
 local function layout1() os.execute("fish -c layout_tabbed") end
                                                                                    
