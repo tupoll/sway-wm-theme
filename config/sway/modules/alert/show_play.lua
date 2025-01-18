42d7338cba3e6fed93b622228a934670a57057ee
tupoll <tupollgbt@gmail.com>
2025-01-18 19:11:50 +1000

    Delete config/sway/modules/alert/show_play.lua

--- config/sway/modules/alert/show_play.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua
-
-io.popen("lua ~/.config/sway/modules/alert/alert_play.lua")
-local a = ("#!/usr/bin/lua\n")
-local b = ('io.popen("lua ~/.config/sway/modules/alert/show_playlist.lua")')
-vol = io.open(".config/sway/modules/alert/alert_play_ch.lua", "w")
-vol:write(a, b)
-vol:close()
-
