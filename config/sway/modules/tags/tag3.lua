c90013f318edb180250a909101531ceb292b6fc4
tupoll <tupollgbt@gmail.com>
2025-01-18 19:32:26 +1000

    tag3.lua

--- config/sway/modules/tags/tag3.lua
+++ config/sway/modules/tags/tag3.lua
@@ -1,12 +1,6 @@
-
-
-local res = os.getenv("HOME") .. "/.config/sway/modules/sway/pictures/"
-
-
-local tags = os.getenv("HOME") .. "/.config/sway/modules/tags"
-
-local function tag_1(sway) os.execute("sway  workspace 3️⃣ fm && setwallpaper /var/tmp/sway/pictures/tag3.jpg &")end 
-local function layout1() os.execute("fish -c layout_tabbed") end
+#!/usr/bin/lua
+local function tag_1(sway) os.execute("sway  workspace 3️⃣ work && setwallpaper /var/tmp/sway/pictures/tag3.jpg &")end 
+local function layout1() os.execute("fish -c layout_splith") end
                                                                                     
 tag_1()       
 layout1()
@@ -14,3 +8,4 @@ local a = ("tag 3")
 vol = io.open("/var/tmp/sway/tag", "w")
 vol:write(a)
 vol:close()
+
