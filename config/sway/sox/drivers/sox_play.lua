f19fa0448edc4f01de167c05782ecf6c1a1dfb18
tupoll <tupollgbt@gmail.com>
2025-01-18 20:36:09 +1000

    Player.

--- config/sway/sox/drivers/sox_play.lua
+++ config/sway/sox/drivers/sox_play.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/lua
+#!/usr/bin/lua
 
 local function play() os.execute("~/tmp/sox/play")end
           
@@ -7,12 +8,13 @@ local b = ('mkdir -p ~/tmp/sox\n')
 local c = ("cp -R ~/tmp/playlist ~/tmp/sox/playlist\n")
 local d = ("cat ~/tmp/sox/playlist |sed '1d'|  /usr/bin/awk '{print $0}'>~/tmp/playlist\n")
 local e = ('head -n 1 ~/tmp/sox/playlist | tail -n1 >~/tmp/sox/name\n')
-local j = ("cat ~/tmp/sox/name>>~/tmp/playlist1\n")
-local f = ("set A $(cat ~/tmp/sox/name |  /usr/bin/awk '{print $0}')\n")
-local h = ("play $A rate -v 48000 bass +15 treble +12\n")
-local l = ("chmod 755 ~/tmp/sox/play \n")
-local p = ("~/tmp/sox/play")
+local f = ("cat ~/tmp/sox/name>>~/tmp/playlist1\n")
+local g = ("cat ~/tmp/playlist | cut -c27-100>/var/tmp/sway/playlist\n")
+local h = ("set A $(cat ~/tmp/sox/name |  /usr/bin/awk '{print $0}')\n")
+local i = ("play $A rate -v 48000 bass +15 treble +12\n")
+local j = ("chmod 755 ~/tmp/sox/play \n")
+local k = ("~/tmp/sox/play")
 vol = io.open("tmp/sox/play", "w")
-vol:write(a, b, c, d, e, j, f, h, l, p)
+vol:write(a, b, c, d, e, f, g, h, i, j, k)
 vol:close()
 play()                                      
