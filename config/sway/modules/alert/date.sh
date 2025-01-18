e8b3627c98f856c1bb697d45fdcc203d45460654
tupoll <tupollgbt@gmail.com>
2025-01-18 19:10:03 +1000

    Delete config/sway/modules/alert/date.sh

--- config/sway/modules/alert/date.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/fish
-set time $(LC_ALL=ru_RU.UTF-8 TZ='Asia/Vladivostok' date +'%H:%M')
-#set time $(lua ~/data.lua)
-echo '<span size="30000" foreground="#998000" face="monospace" weight="bold">Владивосток '$time'</span>'
-
