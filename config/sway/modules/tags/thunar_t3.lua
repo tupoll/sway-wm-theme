9a83dc2e707ff935a8ef5980ab34c966044cba8a
tupoll <tupollgbt@gmail.com>
2025-01-18 19:42:59 +1000

    Delete config/sway/modules/tags/thunar_t3.lua

--- config/sway/modules/tags/thunar_t3.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/lua
-
-local function tag(sway) os.execute("fish -c tag3")end 
---local function layout() os.execute("sway layout tabbed &") end
-local function thunar() os.execute("thunar &")end
-tag()
-thunar()
---layout()
