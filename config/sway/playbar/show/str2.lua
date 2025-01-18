31e91cc12ff98fdd6e9259e0e9a7877c2b03e289
tupoll <tupollgbt@gmail.com>
2025-01-18 18:21:04 +1000

    Positions in the playlist.

---
+++ config/sway/playbar/show/str2.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/lua
+local file = ("/var/tmp/sway/playlist") 
+  local t= {}
+  for line in io.lines(file) do 
+    t[#t+ 1] = line
+  end
+local b = {t[2]}
+
+for key, value in pairs(b) do
+    print( value)
+end
