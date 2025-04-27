13950b6c6923262b38eb533595d7b042cf5479b6
tupoll <tupollgbt@gmail.com>
2025-04-27 05:23:35 +0000

    Update layout_splith.lua

--- config/sway/modules/tags/layouts/layout_splith.lua
+++ config/sway/modules/tags/layouts/layout_splith.lua
@@ -1,10 +1,6 @@
 #!/usr/bin/lua
-
-local function layout() io.popen("sway layout splith &")end
-
-layout()
-
+cmd1 = string.format("sway %s", "layout splith")
+io.popen(cmd1)
 local a = ("splith")
-vol = io.open("/var/tmp/sway/layout", "w")
+vol = io.open("/var/tmp/wm/layout", "w")
 vol:write(a)
-vol:close()
