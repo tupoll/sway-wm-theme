c306c74b3c2b96d8f95a072b862b1b5e14a1b3f2
tupoll <tupollgbt@gmail.com>
2025-04-27 05:20:12 +0000

    Update layout_alt.lua

--- config/sway/modules/tags/layouts/layout_alt.lua
+++ config/sway/modules/tags/layouts/layout_alt.lua
@@ -1,16 +1,17 @@
 #!/usr/bin/lua
 
 local a = ("alt")
-vol = io.open("/var/tmp/sway/layout", "w")
+vol = io.open("/var/tmp/wm/layout", "w")
 vol:write(a)
 vol:close()
-local function alt() io.popen("$HOME/.config/sway/modules/tags/layouts/alternating_layouts.py")end
-alt()
-local function show() io.popen("lua $HOME/.config/sway/modules/tags/layouts/show_layout.lua")end
-show()
+cmd1 = string.format("exec %s", "$HOME/.config/sway/modules/tags/layouts/alternating_layouts.py")
+cmd2 = string.format("lua %s", "$HOME/.config/sway/modules/tags/layouts/show_layout.lua")
+
+io.popen(cmd1)
+io.popen(cmd2)
+
 local b = ('#!/usr/bin/lua\n')
 local c = ('local function kil() io.popen("lua $HOME/.config/sway/modules/tags/layouts/layout_alt_kil.lua")end\n')
 local d = ('kil()')
 change = io.open(".config/sway/modules/tags/layouts/layout_alt_ch.lua", "w")
 change:write(b, c, d)
-change:close()
