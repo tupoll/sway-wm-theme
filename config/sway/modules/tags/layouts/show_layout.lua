c204453e1a5469114dd5f82cdbc252ff8d181744
tupoll <tupollgbt@gmail.com>
2025-04-27 05:29:45 +0000

    Update show_layout.lua

--- config/sway/modules/tags/layouts/show_layout.lua
+++ config/sway/modules/tags/layouts/show_layout.lua
@@ -1,9 +1,6 @@
 #!/usr/bin/lua
-local function kil() io.popen('sleep 2 && pkill -f nwg-wrapper')
-return kil
-end
-local  function layout() io.popen('nwg-wrapper -s layout.sh -r 500 -c timezones.css -p center -mr 50 -a start -mt 500 -j center --layer 3')
-return layout
-end
-kil()
-layout(kil)
+cmd1 = string.format("sleep %s", "2 && pkill -f nwg-wrapper")
+cmd2 = string.format("nwg-wrapper %s", "-s layout.sh -r 500 -c timezones.css -p center -mr 50 -a start -mt 500 -j center --layer 3")
+
+io.popen(cmd1)
+io.popen(cmd2)
