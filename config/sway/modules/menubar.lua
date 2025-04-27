693509b7ad730477bb76ccfbae3fcd33b33b64e3
tupoll <tupollgbt@gmail.com>
2025-04-27 04:47:07 +0000

    Update menubar.lua

--- config/sway/modules/menubar.lua
+++ config/sway/modules/menubar.lua
@@ -1,14 +1,14 @@
 #!/usr.bin/lua
-local function kil() os.execute('killall waybar')end
-local function menubar() os.execute('exec waybar -c $HOME/.config/sway/menubar/config &') end
-kil()
-menubar()
+cmd1 = string.format("killall %s", "ironbar")
+cmd2 = string.format("waybar %s", "-c $HOME/.config/sway/menubar/config")
+
+io.popen(cmd1)
+io.popen(cmd2)
 ------waybar-----
 local a = ("#!/usr/bin/lua\n")
 local b = ("local function kil(sway) os.execute('killall waybar')end\n")
-local c = ("local function waybar(sway) os.execute('lua $HOME/.config/sway/modules/waybar.lua')end\n")
+local c = ("local function waybar(sway) os.execute('lua $HOME/.config/sway/modules/ironbar.lua')end\n")
 local d = ('kil()\n')
 local e = ('waybar()')
 vol = io.open(".config/sway/modules/bar_ch.lua", "w")
 vol:write(a, b, c, d, e)
-vol:close()
