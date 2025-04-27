a95c276e3cb3657ee39d9a3f44bb20b882d4f79f
tupoll <tupollgbt@gmail.com>
2025-04-27 05:22:23 +0000

    Update layout_alt_kil.lua

--- config/sway/modules/tags/layouts/layout_alt_kil.lua
+++ config/sway/modules/tags/layouts/layout_alt_kil.lua
@@ -1,17 +1,17 @@
 #!/usr/bin/lua
 local d = ("alt off")
-vol = io.open("/var/tmp/sway/layout", "w")
+vol = io.open("/var/tmp/wm/layout", "w")
 vol:write(d)
 vol:close()
 
-local function pkill() io.popen("pkill -f $HOME/.config/sway/modules/tags/layouts/alternating_layouts.py")end
-pkill()
+cmd1 = string.format("pkill %s", "-f $HOME/.config/sway/modules/tags/layouts/alternating_layouts.py")
+cmd2 = string.format("lua %s", "$HOME/.config/sway/modules/tags/layouts/show_layout.lua")
+
+io.popen(cmd1)
 local a = ('#!/usr/bin/lua\n')
 local b = ('local function alt() io.popen("lua $HOME/.config/sway/modules/tags/layouts/layout_alt.lua")end\n')
 local c = ('alt()')
 vol = io.open(".config/sway/modules/tags/layouts/layout_alt_ch.lua", "w")
 vol:write(a, b, c)
 vol:close()
-
-local function show() io.popen("lua $HOME/.config/sway/modules/tags/layouts/show_layout.lua")end
-show()
+io.popen(cmd2)
