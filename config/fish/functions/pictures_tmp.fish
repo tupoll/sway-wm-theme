93c035ec943032ccb6cfa721189b1334fb283f69
tupoll <tupollgbt@gmail.com>
2025-04-27 06:15:51 +0000

    Update pictures_tmp.fish

--- config/fish/functions/pictures_tmp.fish
+++ config/fish/functions/pictures_tmp.fish
@@ -1,4 +1,4 @@
-function pictures_tmp --wraps='cp -R .config/sway/pictures /var/tmp/sway/pictures' --description 'alias pictures_tmp cp -R .config/sway/pictures /var/tmp/sway/pictures'
-  cp -R .config/sway/pictures /var/tmp/sway/pictures $argv
+function pictures_tmp --wraps='cp -R .config/sway/pictures /var/tmp/wm/pictures' --description 'alias pictures_tmp cp -R .config/sway/pictures /var/tmp/wm/pictures'
+  cp -R .config/sway/pictures /var/tmp/wm/pictures $argv
         
 end
