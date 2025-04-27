aabebeafff8ad33379fb419b66b598a12108505f
tupoll <tupollgbt@gmail.com>
2025-04-27 04:25:07 +0000

    tag0

--- config/sway/modules/tags/tag0.lua
+++ config/sway/modules/tags/tag0.lua
@@ -1,10 +1,11 @@
 #!/usr/bin/lua
-local function tag_1(sway) os.execute("sway workspace  0️⃣   my && setwallpaper /var/tmp/sway/pictures/tag0.jpg &")end 
-local function layout1() os.execute("fish -c layout_splith") end
-                                                                                    
-tag_1()       
-layout1()
-local a = ("tag 0")
-vol = io.open("/var/tmp/sway/tag", "w")
-vol:write(a)
-vol:close()
+local i = ("0️⃣   my")
+vol = io.open("/var/tmp/wm/tag", "w")
+vol:write(i)
+
+cmd1 = string.format("sway %s", "workspace  0️⃣   my")  
+cmd2 = string.format("setwallpaper %s", "/var/tmp/wm/pictures/tag0.jpg")
+cmd3 = string.format("fish -c %s", "layout_splith")
+io.popen(cmd1)
+io.popen(cmd2)
+io.popen(cmd3)
