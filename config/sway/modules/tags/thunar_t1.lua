c6ecea40a83cd307cc8ce643f58447db3d4dc81c
tupoll <tupollgbt@gmail.com>
2025-04-27 04:42:06 +0000

    Update thunar_t1.lua

--- config/sway/modules/tags/thunar_t1.lua
+++ config/sway/modules/tags/thunar_t1.lua
@@ -1,6 +1,9 @@
 #!/usr/bin/lua
 
-local function tag(sway) io.popen("fish -c tag1")end 
-local function thunar() io.popen("thunar")end
-tag()
-thunar()
+cmd1 = string.format("sway %s", "layout tabbed")
+cmd2 = string.format("setwallpaper %s", "/var/tmp/wm/pictures/tag1.jpg")
+cmd3 = string.format("exec %s", "thunar")
+
+io.popen(cmd1)
+io.popen(cmd2)
+io.popen(cmd3)
