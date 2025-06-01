c1446455505905fe8abdcc1b1a166848de9fc8f5
tupoll <tupollgbt@gmail.com>
2025-06-01 03:25:35 +0000

    Create str0.lua

---
+++ config/sway/menubar/str/str0.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/lua
+
+local file = ("/var/tmp/wm/tag") 
+  local t= {}
+  for line in io.lines(file) do 
+    t[#t+ 1] = line
+  end
+
+local results = string.gmatch(t[1], "0")
+for w in results  do
+   print("🎧")
+end
+
+print(" ")
