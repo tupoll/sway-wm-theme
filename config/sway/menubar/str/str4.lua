76ece7c9f8fd2013d36e61c56564738cb9a1cc3a
tupoll <tupollgbt@gmail.com>
2025-06-01 03:27:20 +0000

    str.lua

---
+++ config/sway/menubar/str/str4.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+local file = ("/var/tmp/wm/tag") 
+  local t= {}
+  for line in io.lines(file) do 
+    t[#t+ 1] = line
+  end
+
+local results = string.gmatch(t[1], "4")
+for w in results  do
+   print("📝")
+end
+
+print(" ")
---
+++ config/sway/menubar/str/str6.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+local file = ("/var/tmp/wm/tag") 
+  local t= {}
+  for line in io.lines(file) do 
+    t[#t+ 1] = line
+  end
+
+local results = string.gmatch(t[1], "6")
+for w in results  do
+   print("📖")
+end
+
+print(" ")
